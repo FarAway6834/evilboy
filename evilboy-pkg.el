616dab617243e26f010e26062bbeba12f3fc514a
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2024-10-19 16:13:57 +0900

    Create evilboy-pkg.el

---
+++ evilboy-pkg.el
@@ -0,0 +1,6 @@
+(define-package "evilboy" "0.1.0" "Evil mode configuration"
+  '((emacs "24.1")
+    (evil "1.2.14"))
+  :keywords '("evil" "configuration")
+  :url "https://github.com/FarAway6834/evilboy"
+  :license "MIT")
